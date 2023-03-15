ab3edd15b44229d3d44b88dca422a605bbb2b79a
gabrielgog <gabrielgog96@gmail.com>
2023-03-15 01:53:53 +0100

    updates

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('Hello test on prod with reverse proxy')
 })
 
 app.listen(PORT, () => console.log('app is running on', PORT))
